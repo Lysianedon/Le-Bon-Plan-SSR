4aca260d96c7412c0986a581100be185e9442db5
lysiandon <don.lysiane@gmail.com>
2022-04-06 12:54:07 +0200

    debugging postgres connexion

--- index.js
+++ index.js
@@ -1,11 +1,45 @@
 const express = require("express");
 const handlebars = require("express-handlebars");
 const port = 8000;
+const cors = require('cors');
+//Libraries
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const Joi = require('Joi');
+const { joiPassword } = require('joi-password');
+
+const dotenv = require("dotenv");
 const path = require("path");
 const cookieParser = require("cookie-parser");
 const app = express();
 const Users = require('./routes/Users');
 const Products = require('./routes/Products');
+
+//dotenv config
+dotenv.config({
+    path: "../config.env",
+});
+
+//Postgress config
+const pg = require('pg');
+const { Pool, Client } = require("pg");
+const Postgres = new Pool({ ssl: { rejectUnauthorized: false }});
+
+// const client = new Client({
+//     host: process.env.PGHOST,
+//     user : process.env.PGUSER,
+//     port : process.env.PGPORT,
+//     password : process.env.PGPASSWORD,
+//     database : process.env.PGDATABASE
+// })
+
+// client.connect(err => {
+//     if (err) {
+//       console.error('connection error', err.stack)
+//     } else {
+//       console.log('connected to Postgres')
+//     }
+//   })
 
 // Handlebars config
 app.engine("handlebars", handlebars.engine());
@@ -23,7 +57,10 @@ app.use(express.json());
 app.use('/users', Users);
 app.use('/products', Products);
 
-
+const debug = app.use((req,res,next) => {
+    console.log("request received.");
+    next();
+})
 
 // ------------------------------- Routes ------------------------------------
 // -------------------------- localhost:8000/ --------------------------------
@@ -37,10 +74,11 @@ app.get('/login', (req,res)=> {
     res.render('login')
 })
 
-app.post('/login', (req,res)=> {
+app.post('/login', async (req,res)=> {
     let userInfos = req.body;
     console.log(userInfos);
-
+    let users = await Postgres.query('SELECT * FROM users');
+    return res.json(users);
 })
 
 // app.post('/signup', (req,res)=> {
